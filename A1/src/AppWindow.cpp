5bb01d42673afedeadd7770455307159bff8aece
Andrew Chin <a8chin@uwaterloo.ca>
2014-09-23 20:24:10 -0400

    Add Menu items to app

--- A1/src/AppWindow.cpp
+++ A1/src/AppWindow.cpp
@@ -33,26 +33,26 @@ void AppWindow::keyPressEvent(QKeyEvent *event) {
     if (event->key() == Qt::Key_Escape) {
         QCoreApplication::instance()->quit();
     }
-    else if (event->key() == Qt::Key_N) {
-        m_game->reset();
-    }
-    else if (event->key() == Qt::Key_R) {
-       m_viewer->resetWorld();
-    }
+    // else if (event->key() == Qt::Key_N) {
+    //     m_game->reset();
+    // }
+    // else if (event->key() == Qt::Key_R) {
+    //    m_viewer->resetWorld();
+    // }
     else if (event->key() == Qt::Key_Q) {
         QCoreApplication::instance()->quit();
     }
 
     // Draw Mode Controls
-    else if (event->key() == Qt::Key_W) {
-        m_viewer->setWireframeMode();
-    }
-    else if (event->key() == Qt::Key_F) {
-       m_viewer->setFillMode();
-    }
-    else if (event->key() == Qt::Key_M) {
-      m_viewer->setMulticolourMode();
-    }
+    // else if (event->key() == Qt::Key_W) {
+    //     m_viewer->setWireframeMode();
+    // }
+    // else if (event->key() == Qt::Key_F) {
+    //    m_viewer->setFillMode();
+    // }
+    // else if (event->key() == Qt::Key_M) {
+    //   m_viewer->setMulticolourMode();
+    // }
 
 
     // Game Controls
@@ -85,16 +85,83 @@ void AppWindow::createActions() {
 
     // Connect the action with the signal and slot designated
     connect(quitAct, SIGNAL(triggered()), this, SLOT(close()));
+
+    QAction* newGame = new QAction(tr("&New Game"), this);
+    QList<QKeySequence> shortcuts;
+    shortcuts.append(tr("N"));
+    newGame->setShortcuts(shortcuts);
+    shortcuts.clear();
+    newGame->setStatusTip(tr("Starts a new game"));
+    connect(newGame, SIGNAL(triggered()), this, SLOT(resetGame()));
+    m_menu_actions.push_back(newGame);
+
+    QAction* resetView = new QAction(tr("&Reset View"), this);
+    shortcuts.append(tr("R"));
+    resetView->setShortcuts(shortcuts);
+    shortcuts.clear();
+    resetView->setStatusTip(tr("Resets game view"));
+    connect(resetView, SIGNAL(triggered()), this, SLOT(resetView()));
+    m_menu_actions.push_back(resetView);
+
+    QAction* wireFrame = new QAction(tr("&Wire-frame mode"), this);
+    shortcuts.append(tr("W"));
+    wireFrame->setShortcuts(shortcuts);
+    shortcuts.clear();
+    wireFrame->setStatusTip(tr("Sets draw mode to wire-frame"));
+    connect(wireFrame, SIGNAL(triggered()), this, SLOT(wireFrame()));
+    m_draw_actions.push_back(wireFrame);
+
+    QAction* fillFace = new QAction(tr("&Fill-face mode"), this);
+    shortcuts.append(tr("F"));
+    fillFace->setShortcuts(shortcuts);
+    shortcuts.clear();
+    fillFace->setStatusTip(tr("Sets draw mode to fill-face mode"));
+    connect(fillFace, SIGNAL(triggered()), this, SLOT(fillFace()));
+    m_draw_actions.push_back(fillFace);
+
+    QAction* multiColored = new QAction(tr("&Multicolored mode"), this);
+    shortcuts.append(tr("M"));
+    multiColored->setShortcuts(shortcuts);
+    shortcuts.clear();
+    multiColored->setStatusTip(tr("Sets draw mode to multicolored mode"));
+    connect(multiColored, SIGNAL(triggered()), this, SLOT(multiColored()));
+    m_draw_actions.push_back(multiColored);
 }
 
 void AppWindow::createMenu() {
     m_menu_app = menuBar()->addMenu(tr("&Application"));
+    m_draw_app = menuBar()->addMenu(tr("&Draw Mode"));
 
     for (auto& action : m_menu_actions) {
         m_menu_app->addAction(action);
+    }
+
+    for (auto& action : m_draw_actions) {
+        m_draw_app->addAction(action);
     }
 }
 
 void AppWindow::gameTick() {
     m_game->tick();
 }
+
+void AppWindow::resetGame() {
+    m_game->reset();
+}
+
+void AppWindow::resetView() {
+    m_viewer->resetWorld();
+}
+
+void AppWindow::wireFrame() {
+    m_viewer->setWireframeMode();
+}
+
+void AppWindow::fillFace() {
+    m_viewer->setFillMode();
+}
+
+void AppWindow::multiColored() {
+    m_viewer->setMulticolourMode();
+}
+
--- A1/src/AppWindow.hpp
+++ A1/src/AppWindow.hpp
@@ -20,20 +20,29 @@ public:
 protected:
     void keyPressEvent(QKeyEvent *event);
 
+private slots:
+    void resetGame();
+    void resetView();
+    void gameTick();
+    void wireFrame();
+    void fillFace();
+    void multiColored();
+
 private:
     void createActions();
     void createMenu();
 
     // Each menu itself
     QMenu* m_menu_app;
+    QMenu* m_draw_app;
+
 
     std::vector<QAction*> m_menu_actions;
+    std::vector<QAction*> m_draw_actions;
     Viewer* m_viewer;
     Game* m_game;
     QTimer* m_gameTimer;
 
-private slots:
-    void gameTick();
 };
 
 #endif
