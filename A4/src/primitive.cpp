a2874a5ba83f9bd98bb44be638aea0999c27664a
Andrew <chin,andr3w@gmail.com>
2014-10-28 12:24:41 -0400

    add retval for primitive intersect

--- A4/src/primitive.cpp
+++ A4/src/primitive.cpp
@@ -63,7 +63,7 @@ Intersection NonhierSphere::intersect(Ray r){
 	Vector3D origin = r.origin;
 	Vector3D dir = r.direction;
 	Vector3D normal;	
-
+//Attempt 2
 // Vector3D center = (Point3D(r.origin[0],r.origin[1],r.origin[2]) - m_pos);
 
 // float t1 = dir.dot(dir);
@@ -83,53 +83,54 @@ Intersection NonhierSphere::intersect(Ray r){
 //     intersection.hit = true;
 //   }
 // }
+  //Attempt 3
+  float bsq = pow(r.direction.dot(r.origin - m_pos), 2);
+  float ac = r.direction.length2() * ((r.origin - m_pos).length2() - pow(m_radius,2) ) ;
+  float underRoot = bsq - ac;
+  if(underRoot < 0.001f){
+    return intersection;
+  }
+  float negVal = -sqrt(underRoot);
+  float posVal = sqrt(underRoot);
 
-  // float bsq = pow(r.direction.dot(r.origin - m_pos), 2);
-  // float ac = r.direction.length2() * ((r.origin - m_pos).length2() - pow(m_radius,2) ) ;
-  // float underRoot = bsq - ac;
-  // if(underRoot < 0.001f){
-  //   return intersection;
-  // }
-  // float negVal = -sqrt(underRoot);
-  // float posVal = sqrt(underRoot);
+  float t0 = (-(r.direction.dot(r.origin-m_pos)) + negVal) / r.direction.length2();
+  float t1 = (-(r.direction.dot(r.origin-m_pos)) + posVal) / r.direction.length2();
+  float t = -1;
+  if ((t0 > 0.1f) )            { t = t0; } 
+  if ((t1 > 0.1f) && (t1 < t)) { t = t1; }
+  if(t > 0){
+    Point3D temp = Point3D(origin[0], origin[1], origin[2]) + dir * t;
+    intersection.point = temp;
+    intersection.normal = temp - m_pos;
+    intersection.normal.normalize();
+    intersection.t = t;
+    intersection.hit = true;
+  }
 
-  // float t0 = (-(r.direction.dot(r.origin-m_pos)) + negVal) / r.direction.length2();
-  // float t1 = (-(r.direction.dot(r.origin-m_pos)) + posVal) / r.direction.length2();
-  // float t = -1;
-  // if ((t0 > 0.1f) )            { t = t0; } 
-  // if ((t1 > 0.1f) && (t1 < t)) { t = t1; }
-  // if(t > 0){
-  //   Point3D temp = Point3D(origin[0], origin[1], origin[2]) + dir * t;
-  //   intersection.point = temp;
-  //   intersection.normal = temp - m_pos;
-  //   intersection.normal.normalize();
-  //   intersection.t = t;
-  //   intersection.hit = true;
-  // }
-
+  //Attempt 1
 	// x = Xe + t(Xp -Xe) ..y..z
 	// x^2 + y^2 + z^2 = 1
-	Vector3D vPos = Vector3D(m_pos);
-	Vector3D dist = vPos - origin; 
-	double B = dir.dot( dist);
-	double D = B*B - dist.dot( dist) + m_radius * m_radius; // A.dot(A) = ||A||^2
+	// Vector3D vPos = Vector3D(m_pos);
+	// Vector3D dist = vPos - origin; 
+	// double B = dir.dot( dist);
+	// double D = B*B - dist.dot( dist) + m_radius * m_radius; // A.dot(A) = ||A||^2
 
-	if (D < 0.0f) 
-	    return intersection; 
-	double t0 = B - sqrt(D); 
-	double t1 = B + sqrt(D);
-	double t = -1;  
+	// if (D < 0.0f) 
+	//     return intersection; 
+	// double t0 = B - sqrt(D); 
+	// double t1 = B + sqrt(D);
+	// double t = -1;  
 
-	if ((t0 > 0.1f) )            { t = t0; } 
-	if ((t1 > 0.1f) && (t1 < t)) { t = t1; }
-	if(t > 0){
-		Point3D temp = Point3D(origin[0], origin[1], origin[2]) + dir * t;
-		intersection.point = temp;
-		intersection.normal = temp - m_pos;
-		intersection.normal.normalize();
-		intersection.t = t;
-		intersection.hit = true;
-	}
+	// if ((t0 > 0.1f) )            { t = t0; } 
+	// if ((t1 > 0.1f) && (t1 < t)) { t = t1; }
+	// if(t > 0){
+	// 	Point3D temp = Point3D(origin[0], origin[1], origin[2]) + dir * t;
+	// 	intersection.point = temp;
+	// 	intersection.normal = temp - m_pos;
+	// 	intersection.normal.normalize();
+	// 	intersection.t = t;
+	// 	intersection.hit = true;
+	// }
 	return intersection;
 }
 
